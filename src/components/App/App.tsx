c4b802a02f330856beb328178f29c90b5c41696e
halasyk_stanislav <galasik_stanislav@icloud.com>
2025-11-02 20:06:44 +0200

    fixed mistake

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import CafeInfo from "../Careinfo/CafeInfo";
+import CafeInfo from "../CafeInfo/CafeInfo";
 import Notification from "../Notification/Notification";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../Votestates/VoteStats";
