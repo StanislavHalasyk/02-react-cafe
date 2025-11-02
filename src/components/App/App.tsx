6a1141017bf3015564c9a747c176c43b4d5408f7
halasyk_stanislav <galasik_stanislav@icloud.com>
2025-11-02 10:25:37 +0200

    HW2

---
+++ src/components/App/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+import CafeInfo from "../Cafeinfo/Cafeinfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../Votestates/VoteStats";
+import Notification from "../Notification/Notification"; // 👈 правильний імпорт
+import type { Votes, VoteType } from "../../types/votes";
+import css from "./App.module.css";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({ ...prev, [type]: prev[type] + 1 }));
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const total = votes.good + votes.neutral + votes.bad;
+  const positive = total > 0 ? Math.round((votes.good / total) * 100) : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={total > 0}
+      />
+      {total > 0 ? (
+        <VoteStats votes={votes} total={total} positive={positive} />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
